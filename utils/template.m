e577c42b713a3a852d54a1717130d602405ff752
dombas <davidrb093g@gmail.com>
2021-11-25 13:29:07 +0100

    create function template

---
+++ utils/template.m
@@ -0,0 +1,48 @@
+function OUT = template(IN1,IN2)
+%FUNCTIONNAME Summary of this function goes here
+%
+%   Usage example OUT = template(IN1)
+%   Detail explanation goes here
+%
+%   Input arguments:
+%  
+%   'ARG1'           Description of the argument. Type and purpose.          
+%  
+%                    Accepted values
+%
+%                    Default: 
+%            
+%  
+%  
+%   Output arguments:
+%  
+%   'ARG1'           Description of the argument. Type and purpose.          
+%  
+%
+%   
+%   Notes
+%   -----
+%   Important usage informationAnother name for a gray-level co-occurrence matrix is a gray-level
+%   spatial dependence matrix.
+%
+%
+%   References
+%   ----------
+%   [1] 
+%
+%   Example 1
+%   ---------      
+%   % Example description
+%
+%     I = [1 1 5 6 8 8;2 3 5 7 0 2; 0 2 3 5 6 7];
+%     [GLCMS,SI] = graycomatrix(I,'NumLevels',9,'G',[])
+%     
+%
+%  
+%   David Romero-Bascones, 2021
+%   dromero@mondragon.edu
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
