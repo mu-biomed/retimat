bc0fbce35fbcdd15ba1e9b57b6c29ee1d4c468cd
David RomBas <50577357+DRomBas@users.noreply.github.com>
2021-12-22 14:00:05 +0100

    Simplify smoothing to only loess

--- structure/morphology/smooth_pit.m
+++ structure/morphology/smooth_pit.m
@@ -1,7 +1,49 @@
-function Z_smooth = smooth_pit(rho, Z, method, varargin)
+function Z_smooth = smooth_pit(rho, Z, span)
+%SMOOTH_PIT Smooth the foveal pit based on LOESS
+%
+%   Z_smooth = smooth_pit(rho, Z, span)
+%   Returnes a smoothed version of Z based on LOESS smoothing.
+%
+%   Input arguments:
+%  
+%   'rho'            Matrix with radial coordinates of thickness values.          
+%  
+%   'Z'              Matrix with foveal pit values           
+%    
+%   'span'           SConfiguration parameter of the smoothing method.
+%
+%
+%   Output arguments:
+%  
+%   'Z_smooth'       Smoothed version of the thickness map.
+%
+%   
+%   Notes
+%   -----
+%   The function expects the data in radial format: [n_angle x n_points].
+%   To use it for entire B-Scans convert data to radial format first.
+%
+%
+%   References
+%   ----------
+%   [1] Romero-Bascones et al., Foveal Pit Morphology Characterization: A 
+%   Quantitative Analysis of the Key Methodological Steps, Entropy, 2021
+%   https://doi.org/10.3390/e23060699
+%
+%
+%   Example 1
+%   ---------      
+%   % Example description
+%
+%     I = [1 1 5 6 8 8;2 3 5 7 0 2; 0 2 3 5 6 7];
+%     [GLCMS,SI] = graycomatrix(I,'NumLevels',9,'G',[])
+%     
+%
+%   David Romero-Bascones, dromero@mondragon.edu
+%   Biomedical Engineering Department, Mondragon Unibertsitatea, 2021
 
-if nargin < 3
-    error("At least 4 input arguments are expected");
+if nargin ~= 3
+    error(['3 input arguments are expected but ' nargin ' where provided']);
 end
 
 % Check the presence of nan values
@@ -14,31 +56,16 @@ n_bscan = n_angle/2;
 
 Z_smooth = nan(n_angle, n_point);
 
-switch method
-    case 'mov_average'
-        n_moving = varargin{1};
-        Z_smooth = moving_average(Z, n_moving);
-    case 'p_spline'
-        lambda = varargin{1};
-        Z_smooth = pSpline(rho, Z, lambda);        
-    case 'loess'
-        span = varargin{1}; 
+for n=1:n_bscan
 
-        for n=1:n_bscan
-            
-            % Reconstruct the B-Scan
-            x = [-fliplr(rho(n+n_bscan,:)) rho(n,2:end)];
-            y = [fliplr(Z(n+n_bscan,:)) Z(n,2:end)]; % Dont get center two times
-            
-            % Smooth the signal
-            y_smooth = smooth(x, y, span/100, 'loess')';
-            
-            % From fitted B-Scan to radial again
-            Z_smooth(n,:) = y_smooth((end-n_point+1):end);% Right part of the B-Scan
-            Z_smooth(n+n_bscan,:) = y_smooth(n_point:-1:1);
-        end
+    % Reconstruct the B-Scan
+    x = [-fliplr(rho(n+n_bscan,:)) rho(n,2:end)];
+    z = [fliplr(Z(n+n_bscan,:)) Z(n,2:end)]; % Dont get center two times
 
-    case 'otherwise'
-        error('Wrong model name');
-end
+    % Smooth the signal
+    z_smooth = smooth(x, z, span, 'loess')';
+
+    % From fitted B-Scan to radial again
+    Z_smooth(n,:) = z_smooth((end-n_point+1):end);% Right part of the B-Scan
+    Z_smooth(n+n_bscan,:) = z_smooth(n_point:-1:1);
 end
--- test/test_morphology.m
+++ test/test_morphology.m
@@ -39,3 +39,16 @@ Thickness = compute_thickness(seg, {'TRT', 'GCIP'}, header.scale_z);
 
 X = get_morph_params(rho, TRT, 'all', true);
 % X = get_morph_params(rho, TRT, {'cft', 'max_slope'}, true);
+
+%% Pit smoothing
+clc;close all;
+file = '../data/raster.vol';
+[header, seg] = read_vol(file, 'coordinates');
+Thickness = compute_thickness(seg, {'TRT', 'GCIP'}, header.scale_z);
+[X, Y, Z] = resample_map(header.X_oct, header.Y_oct, Thickness.GCIP, ...
+    'star', 'n_angle', 24, 'max_d', 2.5, 'n_point', 100);
+[theta, rho] = cart2pol(X, Y);
+
+Z_smooth = smooth_pit(rho, Z, 0.2);
+
+plot(Z','b');hold on;plot(Z_smooth','r')
