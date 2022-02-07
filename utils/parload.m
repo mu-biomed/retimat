4d0790c93b7b440c72c329ae37189873f1c576c9
dombas <davidrb093g@gmail.com>
2022-02-07 18:50:20 +0100

    slight change

--- utils/parload.m
+++ utils/parload.m
@@ -1,10 +1,10 @@
-function varargout = parload(filename,varargin)
+function varargout = parload(file,varargin)
 % parload - load variables from file within a parfor loop
 %
 % varargout = parload(filename,varargin)
 %
 % Input arguments:
-%   filename: .mat file to be open
+%   file: .mat file to be open
 %   varargin: name of the variables to be loaded
 %
 % Output arguments:
@@ -15,27 +15,27 @@ function varargout = parload(filename,varargin)
 
 switch (nargin-1)
     case 1
-        S = load(filename,varargin{1});
+        S = load(file,varargin{1});
     case 2
-        S = load(filename,varargin{1},varargin{2});
+        S = load(file,varargin{1},varargin{2});
     case 3
-        S = load(filename,varargin{1},varargin{2},varargin{3});
+        S = load(file,varargin{1},varargin{2},varargin{3});
     case 4
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4});
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4});
     case 5
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4},...
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4},...
             varargin{5});
     case 6
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4},...
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4},...
             varargin{5},varargin{6});
     case 7
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4},...
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4},...
             varargin{5},varargin{6},varargin{7});
     case 8
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4},...
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4},...
             varargin{5},varargin{6},varargin{7},varargin{8});
     case 9
-        S = load(filename,varargin{1},varargin{2},varargin{3},varargin{4},...
+        S = load(file,varargin{1},varargin{2},varargin{3},varargin{4},...
             varargin{5},varargin{6},varargin{7},varargin{8},varargin{9});
     otherwise
         error('Wrong number of input arguments');
