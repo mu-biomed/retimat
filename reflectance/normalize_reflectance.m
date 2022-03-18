d7e2f060dfbc36d05bc985762659c7ca11a53bc2
dombas <davidrb093g@gmail.com>
2022-03-18 10:12:41 +0100

    Improve bscan reflectance normalization

--- reflectance/normalize_reflectance.m
+++ reflectance/normalize_reflectance.m
@@ -1,18 +1,112 @@
-function Inorm = normalize_reflectance(bscan, top, bottom)
+function bscan_norm = normalize_reflectance(bscan, seg, method)
+%NORMALIZE_REFLECTANCE Normalize B-scan reflectance.
+%
+%   bscan_norm = normalize_reflectance(bscan, seg, method)
+%   Normalize the intensity (reflectance) of one or more bscans based on the
+%   reflectance of the RPE layer.
+%
+%
+%   Input arguments:
+%  
+%   'bscan'          Matrix with B-scans of dimensions n_axial x n_ascan x 
+%                    n_bscan. If 2D matrix then it will be interpreted as a
+%                    single B-scan.
+%  
+%   'seg'            Struct with the segmentation of the ILM, IZ_RPE and BM.
+%  
+%   'method'         Method used to normalize. Use 'ascan' to normalize each
+%                    ascan separately and 'bscan' to normalize each bscan as a
+%                    whole. Default: 'bscan'.
+%
+%  
+%   Output arguments:
+%  
+%   'bscan_norm'     Normalized bscan images.          
+%  
+%   
+%   Notes
+%   -----
+%   This function relies on an accurate segmentation of the ILM, IZ_RPE and BM.
+%
+%   The normalization sets the low and hight reflectance references to be the
+%   vitreous and the RPE, respectively. Can be thought as the % of deviation
+%   from those parametesr. Values outside [0,100] are expected.
+%
+%   The performance might be somewhat slow due to the necessity of loopting 
+%   through all the A-scans. There might be computational improvements using 
+%   poly2mask but that needs to be adapted to handle NaNs properly (usual in
+%   segmentation). It might be also cleaner to use here reflectance_map to
+%   avoid duplicated code.
+%
+%
+%   References
+%   ----------
+%   [1] Sharafeldeen, Precise higher‐order refectivity and morphology models
+%   for early diagnosis of diabetic retinopathy using OCT images, Scientific
+%   Reports, 2021. https://doi.org/10.1038/s41598-021-83735-7
+%
+%
+%   Example
+%   ---------      
+%   % Load a .vol file and normalize the reflectance
+%
+%     [~,seg,bscan] = read_vol('my_file.vol');
+%     bscan_norm = normalize_reflectance(bscan, seg);
+%
+%
+%  
+%   David Romero-Bascones, dromero@mondragon.edu
+%   Biomedical Engineering Department, Mondragon Unibertsitatea, 2022
 
-
-[~, n_ascan, n_bscan] = size(bscan);
-
-I = nan(n_bscan, n_ascan);
-
-for i_bscan=1:n_bscan
-   
-    % Get reference layer reflectance
-    ref_reflectance = get_layer_intensity(bscan(:,:,i_bscan), top(i_bscan,:), ...
-        bottom(i_bscan,:));
-    
-    ref_reflectance = mean(ref_reflectance);
-    
-    bscan(:,:,i_bscan) = bscan(:,:,i_bscan)/ref_reflectance;
-    
+if ismatrix(b_scan)
+    [n_axial, n_ascan] = size(bscan);
+    n_bscan = 1;
+elseif ndims(bscan) == 3
+    [n_axial, n_ascan, n_bscan] = size(bscan);    
+else
+    error('Input b-scan must be either a 2D or a 3D matrix');
 end
+
+bscan_norm = nan(n_axial, n_ascan, n_bscan);
+
+% Round segmentation
+seg.ILM = double(round(seg.ILM));
+seg.IZ_RPE = double(round(seg.IZ_RPE));
+seg.BM = double(round(seg.BM));
+
+for b=1:n_bscan   
+    R_vitreous = nan(1, n_ascan);
+    R_rpe = nan(1, n_ascan);    
+    
+    for a=1:n_ascan
+        % Get RPE and vitreous segmentation
+        ilm = seg.ILM(b, a);
+        bm = seg.BM(b, a);
+        iz_rpe = seg.IZ_RPE(b, a);
+
+        if any(isnan([ilm bm iz_rpe]))
+            warning("NaN values in segmentation. Results might be inaccurate.");
+            continue
+        end
+        
+        % Compute vitreous and RPE reflectance
+        R_vitreous(a) = nanmean(bscan(1:ilm, a, b));
+        R_rpe(a) = nanmean(bscan(iz_rpe:bm, a, b));        
+    end
+    
+    % Normalize based on [1] (we use 100 instead of 1000 here).
+    switch method
+        case 'ascan'
+            bscan_norm(:,:,b) = 100*(bscan(:,:,b) - R_vitreous)./(R_rpe - R_vitreous);
+
+        case 'bscan'
+            R_rpe = nanmean(R_rpe);
+            R_vitreous = nanmean(R_vitreous);
+            
+            bscan_norm(:,:,b) = 100*(bscan(:,:,b) - R_vitreous)/(R_rpe - R_vitreous);         
+        otherwise
+            error("Unknown method. Valid options are: 'ascan' or 'bscan'.");
+    end
+end
+
+bscan_norm = squeeze(bscan_norm);
--- test/test_reflectance.m
+++ test/test_reflectance.m
@@ -7,7 +7,7 @@ file = '../data/raster.vol';
 
 % MR = reflectance_map(bscan, seg, 'mean', 'ILM', 'BM');
 
-Tnfl = reflectance_map(bscan, seg, 'total', 'ILM', 'NFL_GCL');
+Tnfl = reflectance_map(bscan, seg, 'total', 'ILM', 'RNFL_GCL');
 Trpe = reflectance_map(bscan, seg, 'total', 'IDZ_RPE', 'BM');
 
 att = reflectance_map(bscan, seg, 'NFL_RPE_att', header.scale_z, 'ILM', 'BM');
@@ -15,4 +15,24 @@ att = reflectance_map(bscan, seg, 'NFL_RPE_att', header.scale_z, 'ILM', 'BM');
 subplot(131);imagesc(Tnfl);
 subplot(132);imagesc(Trpe);
 subplot(133);imagesc(att);caxis([0 10]);
+colormap(gray);
+
+%% Normalize bscan
+bscan_norm = normalize_reflectance(bscan, seg, 'column');
+bscan_norm2 = normalize_reflectance(bscan, seg, 'bscan');
+
+n_bscan = 6;
+idx_bscan = round(linspace(1,25,n_bscan));
+
+norm_lim = [prctile(bscan_norm(:),1) prctile(bscan_norm(:),99)];
+norm_lim2 = [prctile(bscan_norm2(:),1) prctile(bscan_norm2(:),99)];
+
+for i=1:n_bscan
+    b = idx_bscan(i);
+    
+    subplot(3,n_bscan,i);imagesc(bscan(:,:,b));caxis(minmax(bscan(:)'));
+    subplot(3,n_bscan,i+n_bscan);imagesc(bscan_norm(:,:,b));caxis(norm_lim);
+    subplot(3,n_bscan,i+2*n_bscan);imagesc(bscan_norm2(:,:,b));caxis(norm_lim2);
+end
+
 colormap(gray);
