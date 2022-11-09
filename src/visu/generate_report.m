cbffe0f0c169dbedd806e9a8f810f98753e044b5
David RomBas <50577357+DRomBas@users.noreply.github.com>
2022-11-09 14:02:22 +0100

    improve visualization tutorial and report

--- src/visu/generate_report.m
+++ src/visu/generate_report.m
@@ -30,6 +30,12 @@ function fig = generate_report(bscan, seg, fundus, layers, varargin)
 %                    'file_name': File name to save the plot.
 %                    Default: [] (plot is not saved).
 %
+%                    'visible': 'on' to show the figure window and 'off' to
+%                    hide it. Default: 'on'
+%
+%                    'title': text to us as title.
+%                    Default: [] (no title).
+%
 %
 %   Output arguments:
 %  
@@ -53,7 +59,7 @@ function fig = generate_report(bscan, seg, fundus, layers, varargin)
 %   David Romero-Bascones, dromero@mondragon.edu
 %   Biomedical Engineering Department, Mondragon Unibertsitatea, 2022
 
-[n_plot_bscan, n_col_max, file_name] = parse_inputs(varargin);
+args = parse_inputs(varargin);
 
 % Fundus
 if isempty(fundus)
@@ -71,22 +77,22 @@ end
 
 % Calculate the number of bscans
 if isempty(bscan)
-    n_plot_bscan = 0;
+    args.n_plot_bscan = 0;
     n_en_face = 0;
 else
     n_en_face = 1;
     n_bscan = size(bscan, 3);
-    if n_plot_bscan > n_bscan
-        n_plot_bscan = n_bscan;
+    if args.n_plot_bscan > n_bscan
+        args.n_plot_bscan = n_bscan;
         msg = ['n_plot_bscan is larger than number of b-scans in the image.', ...
                ' Only the maximum number will be plotted'];
         warning(msg);
     end
-    idx_bscan = round(linspace(1, n_bscan, n_plot_bscan));
+    idx_bscan = round(linspace(1, n_bscan, args.n_plot_bscan));
 end
 
 % Number of plots
-n_plot = n_plot_bscan + n_layer + n_fundus + n_en_face;
+n_plot = args.n_plot_bscan + n_layer + n_fundus + n_en_face;
 
 if n_plot == 0
     warning('Not enough data to plot anything');
@@ -94,7 +100,7 @@ if n_plot == 0
 end
 
 % Define figure
-n_col = min([n_col_max n_plot]);
+n_col = min([args.n_col_max n_plot]);
 n_row = ceil(n_plot/n_col);
 
 height = n_row * 300;
@@ -102,7 +108,7 @@ width  = 1500;
 % width = n_col * 300;
 
 % Plotting
-fig = figure('Position',[0 0 width height]);
+fig = figure('Position',[0 0 width height],'Visible', args.visible);
 tiledlayout(n_row, n_col, 'TileSpacing', 'tight');
 
 if n_fundus == 1
@@ -137,7 +143,7 @@ if n_layer > 0
     end
 end
 
-if n_plot_bscan > 0
+if args.n_plot_bscan > 0
     boundaries = fields(seg);
     n_bound = length(boundaries);
     for i=idx_bscan
@@ -150,11 +156,15 @@ if n_plot_bscan > 0
     end
 end
 
-if ~isempty(file_name)
-    saveas(fig, file_name);
+if ~isempty(args.title)
+    sgtitle(args.title, 'Interpreter','None');
 end
 
-function [n_plot_bscan, n_col_max, file_name] = parse_inputs(extra_args)
+if ~isempty(args.file_name)
+    saveas(fig, args.file_name);
+end
+
+function args = parse_inputs(extra_args)
 
 n_args = length(extra_args)/2;
 
@@ -163,9 +173,11 @@ if mod(n_args, 1) ~= 0
 end
 
 % Check arguments
-n_plot_bscan = 5;
-n_col_max    = 5;
-file_name    = [];  
+args.n_plot_bscan = 5;
+args.n_col_max    = 5;
+args.file_name    = [];  
+args.visible      = 'on';
+args.title        = [];
 
 for i=1:n_args
     arg = extra_args{2*i - 1};
@@ -175,16 +187,11 @@ for i=1:n_args
         continue;
     end
     
-    switch arg
-        case 'n_plot_bscan'
-            n_plot_bscan = extra_args{2*i};
-        case 'n_col_max'
-            n_col_max = extra_args{2*i};
-        case 'file_name'
-            file_name = extra_args{2*i};
-        otherwise
-            msg = ['Unknown argument. Valid options are: n_col_max ', ...
-                   'file_name'];
-            warning(msg);
+    if any(strcmp(arg, {'n_plot_bscan','n_col_max','file_name','visible','title'}))
+        args.(arg) = extra_args{2*i};
+    else        
+        msg = ['Unknown argument. Valid options are: n_col_max ', ...
+               'n_plot_bscan','file_name','visible','title'];
+        warning(msg);
     end
 end
