332b77307df7a0e7dc8e6bec4e0904be9a6e1a71
David RomBas <50577357+DRomBas@users.noreply.github.com>
2022-11-09 17:40:37 +0100

    add startup script

---
+++ startup.m
@@ -0,0 +1,23 @@
+% Set-up RETIMAT by adding relevant folders to MATLAB search path
+%
+% David Romero-Bascones, dromero@mondragon.edu
+% Biomedical Engineering Department, Mondragon Unibertsitatea, 2022
+
+try
+    addfcn = @(x) addpath([pwd '/src/' x]);
+
+    addfcn('io');
+    addfcn('reflectance');
+    addfcn('segment');
+    addfcn('spatial');
+    addfcn('structure');
+    addfcn('texture');
+    addfcn('utils');
+    addfcn('visu');
+    
+    disp('RETIMAT is ready to be used.');
+catch emsg
+    disp('Error setting up RETIMAT.');
+    disp(emsg.message);
+end
+
