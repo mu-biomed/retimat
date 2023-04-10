6a41681ad027f2af9ddbd563ef12858c6f3d9fb6
drombas <dromero.research@gmail.com>
2023-04-10 19:59:50 +0200

    add MATLAB doc support

--- doc/conf.py
+++ doc/conf.py
@@ -2,6 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,15 +15,21 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.viewcode', 'sphinxcontrib.matlab','sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 html_static_path = ['_static']
+
+
+# -- MATLAB configuration ----------------------------------------------------
+primary_domain = 'mat'
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+matlab_src_dir = os.path.join(parent_dir, 'src')
+
